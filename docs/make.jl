186949e8d70001c3611bc20ed7e08658c3aedb7f
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:00:43 +0000

    Snapshot of branch master taken on 2019-04-07T21:00:42.484-04:00 from "https://github.com/UnofficialJuliaMirror/OmniSci.jl-085fdc97-bbf0-5359-b689-34bac5cd65e1"

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, OmniSci
+
+makedocs(
+    # options
+    modules = [OmniSci],
+    clean = false,
+    assets = ["assets/favicon.ico"],
+    format = :html,
+    sitename = "OmniSci.jl",
+    pages = [
+        "Getting Started" => "index.md",
+        "Functions" => "functions.md",
+        "API Reference" => "apireference.md"
+
+    ]
+)
+
+deploydocs(
+    repo = "github.com/omnisci/OmniSci.jl.git",
+    #julia = "1.0",
+    deps=nothing,
+    make=nothing,
+    target = "build"
+)
---
+++ src/OmniSci.jl
@@ -0,0 +1,35 @@
+module OmniSci
+
+using Thrift, Random, Sockets, Distributed, DataFrames, Dates, DecFP, GeoInterface, LibGEOS, Decimals
+import Thrift: process, meta, distribute, ThriftMeta, ThriftMetaAttribs
+import Base: show, convert
+import DataFrames: DataFrame
+
+#### types and enums ####
+export TDatum, TColumn, TExecuteMode
+
+#### functions ####
+export connect, disconnect
+export get_status, get_hardware_info
+export get_tables_meta, get_table_details
+export get_users, get_databases
+export sql_execute, sql_execute_df, sql_execute_gdf
+export render_vega
+export get_dashboards, get_dashboard_grantees
+export load_table_binary_columnar, load_table_binary_arrow, load_table
+export create_table, import_table
+export get_roles, get_all_roles_for_user
+export DataFrame
+
+#Thrift interface code, mostly generated from Thrift.jl
+include("thrift_code/enums.jl") #hand-maintained code for more convenient enums
+include("thrift_code/types.jl") #slightly modified from Thrift.jl for circular type fix
+include("thrift_code/client.jl") #same as output by Thrift.jl, except where methods deleted
+
+#Package code
+include("constructors.jl")
+include("ipc.jl")
+include("client.jl")
+include("dataframe_show.jl")
+
+end # module OmniSci
